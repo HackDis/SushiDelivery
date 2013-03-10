19d617c2ff3bf3dbc69f6fbab7d47e8feba98dc6
Roberto <Roberto@Roberto-PC>
2013-03-10 02:33:04 -0400

    Prueba Banche

--- SushiDelivery/src/sushidelivery/SushiDelivery.java
+++ SushiDelivery/src/sushidelivery/SushiDelivery.java
@@ -14,6 +14,6 @@ public class SushiDelivery {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("GitHub");
     }
 }
